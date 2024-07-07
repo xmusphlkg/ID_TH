f171404036afd32999595d631d6de1925a86df1c
xmusphlkg <lkg1116@outlook.com>
2024-07-07 07:59:57 +0000

    add functions

--- Script/GetData.py
+++ Script/GetData.py
@@ -9,30 +9,46 @@ from multiprocessing import Pool
 # Setup logging
 logging.basicConfig(filename='./GetData.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Target URL
-url = 'http://doe.moph.go.th/surdata/index.php'
+def get_diseases(url):
+    """
+    Fetch the list of diseases from the main page.
+    """
+    try:
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logging.error(f"Error fetching the page: {e}")
+        return []
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    disease_list = []
 
-# Send HTTP request to retrieve the webpage content
-response = requests.get(url)
-response.encoding = 'utf-8'
-soup = BeautifulSoup(response.text, 'html.parser')
-disease_list = []
+    # Extract disease name and URL
+    for link in soup.find_all('a', class_='e1'):
+        disease_name = link.text.strip()
+        disease_url = link['href']
+        full_url = requests.compat.urljoin(url, disease_url)
+        historical_url = full_url.replace('disease.php?', 'disease.php?dcontent=old&')
+        disease_list.append((disease_name, full_url, historical_url))
+    
+    # Make folder for each disease
+    for disease_name, _, _ in disease_list:
+        file_path = '../Data/GetData/' + disease_name.replace('/', '_')
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+    
+    return disease_list
 
-# Find all tags that are links with the class 'e1'
-for link in soup.find_all('a', class_='e1'):
-    disease_name = link.text.strip()
-    disease_url = link['href']
-    full_url = requests.compat.urljoin(url, disease_url)
-    historical_url = full_url.replace('disease.php?', 'disease.php?dcontent=old&')
+def add_full_url(full_url, historical_url):
+    """
+    Add the full URL which is missing in the disease URL.
+    """
+    # get file name from historical URL
+    file_name = historical_url.split('/')[-1]
 
-    # Append the disease name and corresponding URLs to the list
-    disease_list.append((disease_name, full_url, historical_url))
-
-# make folder for each disease
-for disease_name, _, _ in disease_list:
-    file_path = '../Data/GetData/' + disease_name.replace('/', '_')
-    if not os.path.exists(file_path):
-        os.makedirs(file_path)
+    # get id from full URL, which between last _ and .
+    id = full_url.split('_')[-1].split('.')[0]
 
 def get_file_url(page_url, file_type = '.rtf'):
     """
@@ -96,10 +112,13 @@ def download_files(disease_name, full_url, history_url):
     file_urls_new = get_file_url(full_url)
 
     with Pool(30) as p:
-        p.starmap(download_file, [(disease_name, file_data, False) for file_data in file_urls_his])
+        # p.starmap(download_file, [(disease_name, file_data, False) for file_data in file_urls_his])
         p.starmap(download_file, [(disease_name, file_data, True) for file_data in file_urls_new])
 
 # Run the script
 if __name__ == '__main__':
+    # Target URL
+    url = 'http://doe.moph.go.th/surdata/index.php'
+    disease_list = get_diseases(url)
     for disease_name, full_url, history_url in disease_list:
         download_files(disease_name, full_url, history_url)
