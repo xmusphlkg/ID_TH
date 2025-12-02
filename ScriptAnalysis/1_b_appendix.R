bd96249608e56c6214ffe249cc9ed188ab66a7b2
xmusphlkg <60965585+xmusphlkg@users.noreply.github.com>
2025-12-02 17:14:59 +0800

    Refactor disease analysis script and add cases plot
    Renamed and updated the disease analysis script to use 'data_month' instead of 'data_analysis', removed commented code, adjusted plot themes, and changed output to save a PNG of cases in the appendix. Also added the new cases plot image to the supplementary appendix.

--- ScriptAnalysis/1_b_appendix.R
+++ ScriptAnalysis/1_b_appendix.R
@@ -18,7 +18,7 @@ load("./month.RData")
 
 # group plot -------------------------------------------------------------
 
-data_plot <- data_analysis |>
+data_plot <- data_month |>
      filter(Shortname %in% data_class$Shortname) |>
      select(Date, Shortname, Group, Cases, Deaths) |>
      mutate(
@@ -77,13 +77,6 @@ for (i in 1:length(disease_groups)) {
                  month = month(Date),
                  value_norm = (Cases - mean(Cases, na.rm = T)) / sd(Cases, na.rm = T),
                  Date = format(ymd(Date), "%Y.%m"))
-     # data_fig[[paste("panel", LETTERS[i * 3 - 0])]] <- data_plot |>
-     #      filter(Group == group_list) |>
-     #      group_by(Shortname) |>
-     #      mutate(year = year(Date),
-     #             month = month(Date),
-     #             value_norm = (Deaths - mean(Deaths, na.rm = T)) / sd(Deaths, na.rm = T),
-     #             Date = format(ymd(Date), "%Y.%m"))
 }
 
 plot_single <- function(i) {
@@ -119,9 +112,8 @@ plot_single <- function(i) {
                 panel.grid.major.y = element_blank(),
                 panel.grid.minor.y = element_blank(),
                 axis.text = element_text(size = 10.5, color = "black"),
-                axis.title.y = element_text(size = 11, color = "black", face = "bold"),
-                plot.title = element_text(face = "bold", size = 14, color = "black"),
-                plot.title.position = "plot") +
+                axis.title.y = element_text(size = 11, color = "black", face = "bold", vjust = -3),
+                plot.title = element_text(face = "bold", size = 14, color = "black")) +
           labs(x = NULL,
                y = "Monthly cases",
                color = NULL,
@@ -148,48 +140,15 @@ plot_single <- function(i) {
           theme(legend.position = "bottom",
                 panel.grid = element_blank(),
                 axis.text = element_text(size = 10.5, color = "black"),
-                plot.title = element_text(face = "bold", size = 14, color = "black"),
-                plot.title.position = "plot") +
+                plot.title = element_text(face = "bold", size = 14, color = "black")) +
           guides(fill = guide_colourbar(barwidth = 10,
-                                        barheight = 0.5,
-                                        color = "black")) +
+                                        barheight = 0.5)) +
           labs(x = "Date",
                y = NULL,
                fill = "Normalized monthly cases",
                title = LETTERS[i * 2])
      
-     # data_plot_deaths <- data_fig[[paste("panel", LETTERS[i * 3])]] |>
-     #      mutate(out_label = if_else(value_norm > 10, "*", ""))
-     # fig3 <- ggplot(data = data_plot_deaths,
-     #                mapping = aes(fill = value_norm, x = Date, y = Shortname)) +
-     #      geom_tile() +
-     #      geom_text(mapping = aes(label = out_label),
-     #                vjust = 0.5) +
-     #      coord_equal(3) +
-     #      scale_fill_gradientn(colors = paletteer_d("rcartocolor::Fall"),
-     #                           trans = log_fill,
-     #                           na.value = 'white',
-     #                           limits = c(-5, 10)) +
-     #      scale_x_discrete(breaks = paste(seq(2007, 2024), "01", sep = "."),
-     #                       labels = 2007:2024,
-     #                       expand = expansion(add = c(0, 0))) +
-     #      scale_y_discrete(limits = rev(data_class$Shortname[data_class$Group == group_list]),
-     #                       expand = c(0, 0)) +
-     #      theme_bw() +
-     #      theme(legend.position = "bottom",
-     #            panel.grid = element_blank(),
-     #            axis.text = element_text(size = 10.5, color = "black"),
-     #            plot.title = element_text(face = "bold", size = 12, color = "black"),
-     #            plot.title.position = "plot") +
-     #      guides(fill = guide_colourbar(barwidth = 10,
-     #                                    barheight = 0.5,
-     #                                    color = "black")) +
-     #      labs(x = "Date",
-     #           y = NULL,
-     #           fill = "Normalized monthly deaths",
-     #           title = LETTERS[i * 3])
-     
-     return(fig1 + fig2 + plot_layout(ncol = 1, heights = c(1, 1)))
+     return(fig1 + fig2 + plot_layout(ncol = 1, heights = c(0.4, 1)))
 }
 
 ## create figure panel for all Group
@@ -198,20 +157,7 @@ fig <- wrap_plots(plot_list, ncol = 1) +
      plot_layout(guides = "collect") &
      theme(legend.position = "bottom")
 
-ggsave(filename = "../outcome/publish/fig6.pdf",
+ggsave(filename = "../outcome/Appendix/Supplementary Appendix 1_1/Cases.png",
        plot = fig,
-       width = 12,
-       height = 35,
-       device = cairo_pdf,
-       family = "Times New Roman")
-
-ggsave(filename = "../outcome/publish/fig6_1.pdf",
-       plot = fig,
-       width = 12,
-       height = 18,
-       device = cairo_pdf,
-       family = "Times New Roman")
-
-# figure data
-write.xlsx(data_fig,
-           file = "../outcome/Appendix/figure_data/fig6.xlsx")
+       width = 14,
+       height = 18)
